8a82ee096fb1528f821e80b11065ddfc9d59f124
Folka2134 <folka2134@gmail.com>
2023-12-23 13:59:20 +0000

    updated default form values

--- constants/index.ts
+++ constants/index.ts
@@ -8,3 +8,17 @@ export const NavLinks = [
     route: "/saved-jobs/"
   },
 ]
+
+export const jobDefaultValues = {
+  imageUrl: '',
+  companyName: "",
+  title: '',
+  description: '',
+  featured: false,
+  roles: [],
+  roleType: "",
+  location: '',
+  datePosted: new Date(),
+  dateUpdated: new Date(),
+  appliedUsers: [],
+}
