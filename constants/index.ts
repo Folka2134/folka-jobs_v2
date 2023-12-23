69d77c27a7ea0716292baf18615c6125b3d0ec9b
Folka2134 <folka2134@gmail.com>
2023-12-23 15:27:10 +0000

    created job roles and updated default values

--- constants/index.ts
+++ constants/index.ts
@@ -13,12 +13,38 @@ export const jobDefaultValues = {
   imageUrl: '',
   companyName: "",
   title: '',
+  location: '',
   description: '',
-  featured: false,
   roles: [],
   roleType: "",
-  location: '',
   datePosted: new Date(),
   dateUpdated: new Date(),
-  appliedUsers: [],
+  featured: false,
 }
+
+export const jobRoles = [
+  {
+    id: "Frontend",
+    label: "Front-End",
+  },
+  {
+    id: "Backend",
+    label: "Back-end",
+  },
+  {
+    id: "Fullstack",
+    label: "Fullstack",
+  },
+  {
+    id: "Devops",
+    label: "Devops",
+  },
+  {
+    id: "Senior",
+    label: "Senior",
+  },
+  {
+    id: "Junior",
+    label: "Junior",
+  },
+] as const;
