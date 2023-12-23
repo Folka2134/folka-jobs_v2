617144b547fdaf4f0ba9e23c99478bbede763c2a
Folka2134 <folka2134@gmail.com>
2023-12-23 15:28:47 +0000

    created job form

--- components/shared/JobForm.tsx
+++ components/shared/JobForm.tsx
@@ -16,7 +16,12 @@ import {
 import { Input } from "@/components/ui/input";
 import { jobFormSchema } from "@/lib/validator";
 import { z } from "zod";
-import { jobDefaultValues } from "@/constants";
+import { jobDefaultValues, jobRoles } from "@/constants";
+import { Textarea } from "../ui/textarea";
+import { Checkbox } from "../ui/checkbox";
+import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
+import { useState } from "react";
+import { FileUploader } from "./FileUploader";
 
 type JobFormProps = {
   userId: string;
@@ -24,6 +29,8 @@ type JobFormProps = {
 };
 
 const JobForm = ({ userId, type }: JobFormProps) => {
+  const [files, setFiles] = useState<File[]>([]);
+
   const initialValues = jobDefaultValues;
 
   const form = useForm<z.infer<typeof jobFormSchema>>({
@@ -40,24 +47,169 @@ const JobForm = ({ userId, type }: JobFormProps) => {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col gap-5"
+      >
+        <div className="flex flex-col gap-5">
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <FileUploader
+                    onFieldChange={field.onChange}
+                    imageUrl={field.value}
+                    setFiles={setFiles}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="companyName"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input
+                    placeholder="Company name"
+                    {...field}
+                    className="input-field"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input
+                    placeholder="Job title"
+                    {...field}
+                    className="input-field"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="location"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input
+                    placeholder="Job location"
+                    {...field}
+                    className="input-field"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Textarea
+                    className="rounded-xl"
+                    placeholder="Job description"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="roles"
+            render={() => (
+              <FormItem>
+                <div className="mb-4">
+                  <FormLabel className="text-base">Job roles</FormLabel>
+                </div>
+                {jobRoles.map((role) => (
+                  <FormField
+                    key={role.id}
+                    control={form.control}
+                    name="roles"
+                    render={({ field }) => {
+                      return (
+                        <FormItem
+                          key={role.id}
+                          className="flex flex-row items-start space-x-3 space-y-0"
+                        >
+                          <FormControl>
+                            <Checkbox
+                              checked={field.value?.includes(role.id)}
+                              onCheckedChange={(checked) => {
+                                return checked
+                                  ? field.onChange([...field.value, role.id])
+                                  : field.onChange(
+                                      field.value?.filter(
+                                        (value: string) => value !== role.id,
+                                      ),
+                                    );
+                              }}
+                            />
+                          </FormControl>
+                          <FormLabel className="font-normal">
+                            {role.label}
+                          </FormLabel>
+                        </FormItem>
+                      );
+                    }}
+                  />
+                ))}
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="roleType"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Role Type</FormLabel>
+                <FormControl>
+                  <RadioGroup
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                    className="flex flex-col gap-2"
+                  >
+                    <FormItem className="flex items-center space-x-3 space-y-0">
+                      <FormControl>
+                        <RadioGroupItem value="Full-time" />
+                      </FormControl>
+                      <FormLabel className="font-normal">Full-time</FormLabel>
+                    </FormItem>
+                    <FormItem className="flex items-center space-x-3 space-y-0">
+                      <FormControl>
+                        <RadioGroupItem value="Part-time" />
+                      </FormControl>
+                      <FormLabel className="font-normal">Part-time</FormLabel>
+                    </FormItem>
+                  </RadioGroup>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <Button type="submit" className="button bg-[#95A2EF]">
+          Submit
+        </Button>
       </form>
     </Form>
   );
