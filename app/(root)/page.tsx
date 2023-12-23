47de26010a914e34d6d96dc2d67e3f355480c01a
Folka2134 <folka2134@gmail.com>
2023-12-23 14:01:55 +0000

    updated homepage with create job link

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,16 @@
 import Navbar from "@/components/shared/Navbar";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Testing mongodb + clerk shared user data</h1>
+      <div className="flex w-full items-center justify-center">
+        <h1 className="flex-1 text-3xl">Browse Jobs</h1>
+        <Link href="/jobs/create" className="">
+          Post job
+        </Link>
+      </div>
     </main>
   );
 }
