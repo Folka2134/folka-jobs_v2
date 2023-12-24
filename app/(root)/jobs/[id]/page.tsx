21286e4fe35a683511de566a73e7c2b405bf5f8b
Folka2134 <folka2134@gmail.com>
2023-12-24 18:53:26 +0000

    updated job page

--- app/(root)/jobs/[id]/page.tsx
+++ app/(root)/jobs/[id]/page.tsx
@@ -1,7 +1,94 @@
-import React from "react";
+import Image from "next/image";
 
-const JobPage = () => {
-  return <div>JobPage</div>;
+import { getJobById } from "@/lib/actions/job.actions";
+import { SearchParamProps } from "@/types";
+import { formatDateTime } from "@/lib/utils";
+
+const JobPage = async ({ params: { id } }: SearchParamProps) => {
+  const job = await getJobById(id);
+
+  const tagColours = ["#95DCF0", "#C295F0", "#95BFF0", "#A495F0", "#95A2EF"];
+
+  return (
+    <section className="flex justify-center bg-gray-50">
+      <div className="grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl">
+        <Image
+          src={job.imageUrl}
+          alt={job.title}
+          width={500}
+          height={800}
+          className="h-full min-h-[300px] object-cover object-center"
+        />
+
+        <div className="flex w-full flex-col gap-8 p-5 md:p-10">
+          <div className="flex flex-col gap-6">
+            <h2 className="text-2xl font-bold">{job.title}</h2>
+            <div className="flex flex-row items-center gap-3">
+              <p className="">
+                by{" "}
+                <span className="text-[#95A2F0]">
+                  {`${job.recruiter.firstName} ${job.recruiter.lastName}`} |{" "}
+                  {job.companyName}
+                </span>
+              </p>
+              <div className="flex gap-3">
+                {job.featured ? (
+                  <span className="rounded-full bg-[#C295F0] px-2 py-1 text-white transition-opacity duration-200 hover:opacity-90">
+                    Featured
+                  </span>
+                ) : (
+                  ""
+                )}
+              </div>
+            </div>
+            <button>Buy Ticket</button>
+
+            <div className="flex flex-col gap-5">
+              <div className="flex gap-2 md:gap-3">
+                <Image
+                  src="/assets/icons/calendar.svg"
+                  alt="calendar"
+                  width={20}
+                  height={20}
+                />
+                <div>
+                  <p className="ml-1">
+                    {formatDateTime(job.datePosted).dateOnly}
+                  </p>
+                </div>
+              </div>
+              <div className="flex gap-2 md:gap-3">
+                <Image
+                  src="/assets/icons/location.svg"
+                  alt="location"
+                  width={20}
+                  height={20}
+                />
+
+                <p className="ml-1">{job.location}</p>
+              </div>
+            </div>
+            <div className="flex flex-col gap-2">
+              <p className="font-bold text-gray-600">Job Description</p>
+              <p className="text-gray-500">{job.description}</p>
+            </div>
+            <div className="flex">
+              <ul>
+                {job.roles.map((role: any, indx: any) => (
+                  <li
+                    key={indx}
+                    className={`bg-[${tagColours[indx]}] rounded-full px-3 py-1 text-white`}
+                  >
+                    {role}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default JobPage;
--- lib/actions/job.actions.ts
+++ lib/actions/job.actions.ts
@@ -6,6 +6,10 @@ import { connectToDatabase } from "../database"
 import User from "../database/models/user.model"
 import Job from "../database/models/job.model"
 
+const getRecruiterDetails = async (query: any) => {
+  return query.populate({ path: "recruiter", model: User, select: "_id firstName lastName" })
+}
+
 export const createJob = async ({ job, userId, path }: CreateJobParams) => {
   try {
     await connectToDatabase()
@@ -20,6 +24,22 @@ export const createJob = async ({ job, userId, path }: CreateJobParams) => {
     const newJob = await Job.create({ ...job, recruiter: userId})
 
     return JSON.parse(JSON.stringify(newJob))
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+export const getJobById = async (jobId: string) => {
+  try {
+    await connectToDatabase()
+
+    const job = await getRecruiterDetails(Job.findById(jobId))
+
+    if (!job) {
+      throw new Error("Job not found")
+    }
+
+    return JSON.parse(JSON.stringify(job))
   } catch (error) {
     handleError(error)
   }
