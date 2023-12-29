40234787e6bbc0612f4907c4d9a6435b2c1841ac
Folka2134 <folka2134@gmail.com>
2023-12-29 14:16:32 +0000

    passing job data to update form

--- app/(root)/jobs/[id]/update/page.tsx
+++ app/(root)/jobs/[id]/update/page.tsx
@@ -1,13 +1,20 @@
 import { DeleteModal } from "@/components/shared/DeleteModal";
 import JobForm from "@/components/shared/JobForm";
-import { SearchParamProps } from "@/types";
+import { SearchParamProps, UpdateJobParams } from "@/types";
 import { auth } from "@clerk/nextjs";
 import React from "react";
 
 import { getJobById } from "@/lib/actions/job.actions";
 
-const UpdateJob = async ({ params: { id } }: SearchParamProps) => {
+type UpdateJobProps = {
+  params: {
+    id: string;
+  };
+};
+
+const UpdateJob = async ({ params: { id } }: UpdateJobProps) => {
   const { sessionClaims } = auth();
+  const job = await getJobById(id);
 
   const userId = sessionClaims?.userId as string;
 
@@ -20,7 +27,7 @@ const UpdateJob = async ({ params: { id } }: SearchParamProps) => {
 
         <div className="wrapper">
           <DeleteModal jobId={id} />
-          <JobForm userId={userId} type="Update" />
+          <JobForm userId={userId} type="Update" job={job} jobId={id} />
         </div>
       </section>
     </>
